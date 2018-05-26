23530d9312815d9b1a5fd88e0f4097af65dcfdad
魏巍 <wewin11235@163.com>
2018-05-26 17:26:05 +0800

    feature(authen): make use can login and redirct to right page

--- src/views/LoginForm.js
+++ src/views/LoginForm.js
@@ -1,22 +1,55 @@
 import React from 'react';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 const FormItem = Form.Item;
+import * as HttpHandler from '../conserns/HttpHandler';
+import AlertIt from './AlertIt';
+import createHistory from 'history/createBrowserHistory';
+const history = createHistory({basename: "/", forceRefresh: true})
 
 class NormalLoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alert_it: false,
+      alert_message: '',
+      alert_type: 'warning',
+    }
+  }
+
+  loginCallbak = (data) => {
+    if(data['status'] === 1) {
+      sessionStorage.setItem('access_token', data['access_token']);
+      history.push('/');
+    } else {
+      this.setState({
+        alert_it: true,
+        alert_message: '登录失败：账号或密码错误！',
+      })
+    }
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        const url = 'http://localhost:3000/api/auth/login';
+        HttpHandler.postHandler(url, values, this.loginCallbak);
       }
     });
   }
+
+  handleClose = () => {
+    this.setState({
+      alert_it: false,
+    });
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <FormItem>
-          {getFieldDecorator('userName', {
+          {getFieldDecorator('login', {
             rules: [{ required: true, message: 'Please input your username!' }],
           })(
             <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}  size="large" placeholder="Username" />
@@ -34,6 +67,10 @@ class NormalLoginForm extends React.Component {
             登录
           </Button>
         </FormItem>
+        {
+          this.state.alert_it &&
+            <AlertIt message={this.state.alert_message} handleClose={this.handleClose} type={this.state.alert_type}/>
+        }
       </Form>
     );
   }
