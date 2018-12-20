bf7d4706c6ffbaa5133e5c349347a3bf30f63bf2
魏巍 <wewin11235@163.com>
2018-12-20 18:36:58 +0800

    feat(sider-nav): generate categories list

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
+import {GetHandler} from '../conserns/HttpHandler';
 
 class CategoryList extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      categories: new Set(),
+    }
+  }
+
+  componentWillMount() {
+    const url = 'api/categories';
+    GetHandler(url, this.callback);
+  }
+
+  callback = (data) => {
+    console.log(data);
+    this.setState({categories: data['categories']});
   }
 
   render() {
     return (
-      <div>
-        Categories List
+      <div className="category-list">
+        <ul>
+          { this.state.categories.length > 0
+            ? this.state.categories.map((item) => {
+                return <li key={`${item.id}`}>{item.name}</li>
+              })
+            : '加载中...'
+          }
+        </ul>
       </div>
-    )
+    );
   }
 }
 
