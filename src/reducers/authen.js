a5bdc497f3f10cb6acfc11cfb8754506ca6f9dc8
魏巍 <wewin11235@163.com>
2019-02-14 10:16:32 +0800

    feat(use-redux): update authen action

--- src/reducers/authen.js
+++ src/reducers/authen.js
@@ -1,31 +1,32 @@
-const authen = (state = {}, action) => {
-  switch(action.type) {
-    case 'START_REQUEST':
-      return Object.assign({}, state, {
-        isLogining: true,
-        show_alert: false,
-      })
-    case 'REQUEST_FAILED':
-      return Object.assign({}, state, {
-        loginFailed: true,
-        message: action.message,
-        show_alert: true,
-      })
-    case 'REQUEST_SUCCESS':
-      return Object.assign({}, state, {
-        loginFailed: false,
-        isLogining: false,
-        loginSuccess: action.loginSuccess,
-        access_token: action.access_token,
-        show_alert: !action.loginSuccess,
-      })
-    case 'TOGGLE_CLOSE_ALERT_ICON':
-      return Object.assign({}, state, {
-        show_alert: false
-      })
-    default:
-      return state
-  }
+const authen =
+  (state = {loginSuccess: !!sessionStorage.access_token}, action) => {
+    switch(action.type) {
+      case 'START_REQUEST':
+        return Object.assign({}, state, {
+          isLogining: true,
+          show_alert: false,
+        })
+      case 'REQUEST_FAILED':
+        return Object.assign({}, state, {
+          loginFailed: true,
+          message: action.message,
+          show_alert: true,
+        })
+      case 'REQUEST_SUCCESS':
+        return Object.assign({}, state, {
+          loginFailed: false,
+          isLogining: false,
+          loginSuccess: action.loginSuccess,
+          access_token: action.access_token,
+          show_alert: !action.loginSuccess,
+        })
+      case 'TOGGLE_CLOSE_ALERT_ICON':
+        return Object.assign({}, state, {
+          show_alert: false
+        })
+      default:
+        return state
+    }
 }
 
 export default authen;
