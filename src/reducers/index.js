354ded083febbe378b267eea895ee233d5552ee5
魏巍 <wewin11235@163.com>
2019-02-17 22:22:06 +0800

    feat(use-redux): rename reducer files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from 'redux';
 
-import article from './article';
-import category from './category';
-import article_detail from './article_detail';
-import main_content from './main_content';
-import new_article from './new_article';
-import authen from './authen';
-import deleteArticle from './delete_article';
+import article from './articleReducer';
+import category from './categoryReducer';
+import article_detail from './articleDetailReducer';
+import main_content from './mainContentReducer';
+import new_article from './newArticleReducer';
+import authen from './authenReducer';
+import deleteArticle from './deleteArticleReducer';
 
 const reducer = combineReducers({
   article,
